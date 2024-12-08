bc6bff0907019073c68e45ab8b7700863adf1c24
Věra Juračáková <juracakova.vera@gmail.com>
2024-12-08 20:37:21 +0100

    Řešení cvičení 08.12.2024

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+let side = 1
+const dice = document.querySelector(".dice")
+const body = document.querySelector("body")
 
+// keydown
+
+body.addEventListener("keydown", () => {
+    side++    
+    if (side > 6) {
+        side = 1  }
+
+    console.log(side)
+    dice.src = `img/side${side}.svg`
+    })
